ebc1cc086183687ae86e20ae6fa2721d9cbeedcc
Abdullah rafiq <abdullahrafiq273@gmail.com>
2023-07-26 16:10:25 +0500

    Add files via upload

---
+++ backbenchers/src/products/Products.js
@@ -0,0 +1,11 @@
+import "./Products.css";
+
+const Products = ({ result }) => {
+  return (
+    <>
+      <section className="card-container">{result}</section>
+    </>
+  );
+};
+
+export default Products;
---
+++ backbenchers/src/sidebar/Sidebar.js
@@ -0,0 +1,21 @@
+import Category from "./category/Category";
+import Price from "./price/Price";
+import Colors from "./colors/Colors";
+import "./Sidebar.css";
+
+const Sidebar = ({ handleChange }) => {
+  return (
+    <>
+      <section className="sidebar">
+        <div className="logo-container">
+          <h1>Shows store</h1>
+        </div>
+        <Category handleChange={handleChange} />
+        <Price handleChange={handleChange} />
+        <Colors handleChange={handleChange} />
+      </section>
+    </>
+  );
+};
+
+export default Sidebar;
